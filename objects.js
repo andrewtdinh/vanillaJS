93b1edadc87d40bcbece86dcf51b2cb9189bcb7d
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-13 16:42:05 -0700

    after object lesson

---
+++ objects.js
@@ -0,0 +1,41 @@
+var dog = {};
+dog.name = 'fido';
+dog.age = 3;
+
+var cat = {name:'fluffy', age:5};
+console.log(dog, cat);
+cat.toys = ['fluff', 'scratch', 'mouse'];
+console.log(cat);
+cat.toys.push('small box');
+console.log(cat);
+cat.toys.splice(1,1);
+console.log(cat);
+
+function Cat(name, age){
+  this.name = name;
+  this.age = age;
+}
+
+Cat.prototype.purr = function(){
+  console.log(this.name +' is purring and it is ' + this.age + ' years old');
+};
+
+var fluffy = new Cat('fluffy', 3);
+console.log('fluffy', fluffy);
+
+// fluffy.purr();
+
+var molly = new Cat('molly', 5);
+// molly.purr();
+
+var cats = [];
+for (var i = 1; i < 11; i++){
+  var cat = new Cat('cat' + i, i);
+  cats.push(cat);
+}
+
+cats.forEach(function(cat){
+  if (cat.age > 5){
+    cat.purr();
+  }
+});
